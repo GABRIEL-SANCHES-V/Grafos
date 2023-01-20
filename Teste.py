69c88dc98ee708b9a5ac7a729a2e0a61fb7d353b
GABRIEL-SANCHES-V <gabriel.vinhal@outlook.com>
2023-01-20 01:17:19 -0300

    Configuações iniciais

--- Teste.py
+++ Teste.py
@@ -1,4 +1,4 @@
-n1  = int(input('numero 01: '))
-n2  = int(input('numero 02: '))
-n3  = int(input('numero 03: '))
+n1 = int(input('numero 01: '))
+n2 = int(input('numero 02: '))
+n3 = int(input('numero 03: '))
 print(n1 + n2 + n3)
